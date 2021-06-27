3cd2720b181b15e9757c5327ea73bab5f7ecf019
kanishka-28 <82566639+kanishka-28@users.noreply.github.com>
2021-06-27 17:49:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./info";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>JavaScript and react.js</h1>
+      <p>A basic webdev react js bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
